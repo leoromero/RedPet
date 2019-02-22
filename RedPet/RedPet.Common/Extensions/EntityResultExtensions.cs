fbfac02f01b04dfd5ae77f3cd0c43a2576c0d175
Leonel Romero <leonel.romero89@gmail.com>
2019-02-22 12:44:45 -0500

    Added extension method to add error messages

---
+++ RedPet/RedPet.Common/Extensions/EntityResultExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Results;
+using RedPet.Common.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedPet.Common.Extensions
+{
+    public static class EntityResultExtensions
+    {
+        public static void AddError<T>(this EntityResult<T> entityResult, string message)
+        {
+            entityResult.Errors.Add(new ValidationFailure(string.Empty, message));
+        }
+    }
+}
